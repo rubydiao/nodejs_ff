846a5669f6f961c49d68ea8ae37007e5e3a7a6eb
rubydiao <narutchai444@gmail.com>
2021-03-17 21:25:33 +0700

    Homework Register & Search

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,31 +1,49 @@
-const fetch = require("node-fetch");
+const fetch = require('node-fetch')
 /**
- * 
- * @param {import("express").Request} req 
- * @param {import("express").Response} res 
+ *
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
  */
-const Login = (req,res) =>{
+let array = []
+const Login = (req, res) => {
     // const {username,pass} = req.query
     // const {username,pass} = req.params
-    const {username,pass} = req.body
+    const { username, pass } = req.body
     res.json({
         username,
-        pass
+        pass,
     })
 }
-const Users = async (req,res) =>{
+const Users = async (req, res) => {
     // const {username,pass} = req.query
     // const {username,pass} = req.params
     // const {msg} = req.body
-    const data = await fetch("https://jsonplaceholder.typicode.com/users")
-    const json = await data.json();
-    const {id} = req.params
-    const val  = json.filter((user) => user.company.name == id)
+    // const data = await fetch('https://jsonplaceholder.typicode.com/users')
+    // const json = await data.json()
+    // const { id } = req.params
+    // const val = json.filter((user) => user.company.name == id)
     res.json({
-        data: val
+        data: array,
     })
 }
-module.exports = {
-    Login , Users
+const Register = (req, res) => {
+    const { name } = req.body
+    array.push({ name })
+    res.json({
+        name,
+    })
 }
-    
+const Search = (req, res) => {
+    const { nameinput } = req.params
+    const val = array.filter((r) => r.name == nameinput)
+    res.json({
+        data: val,
+    })
+}
+
+module.exports = {
+    Login,
+    Users,
+    Register,
+    Search,
+}
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,18 @@
-const express = require("express");
-const {Login,Users} = require("../controllers/user.controller.js");
+const express = require('express')
+const {
+    Login,
+    Users,
+    Register,
+    Search,
+} = require('../controllers/user.controller.js')
 const user_route = express.Router()
 
-user_route.post("/login",(req,res)=> Login(req,res))
-user_route.get('/:id', (req,res) => Users(req,res))
+user_route.post('/login', (req, res) => Login(req, res))
+// user_route.get('/:id', (req, res) => Users(req, res))
+user_route.post('/register', (req, res) => Register(req, res))
+user_route.get('/search/:nameinput', (req, res) => Search(req, res))
+user_route.get('/', (req, res) => Users(req, res))
+
 // user_route.put("/",(req,res)=>{
 //     res.json({
 //         message: "Users PUT"
